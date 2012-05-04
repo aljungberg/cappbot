cd213683cea52d20c3b673e3f27c3850f507a8b9
Alexander Ljungberg <aljungberg@slevenbits.com>
2012-05-04 16:25:49 +0100

    Unit test `current_user` property.

--- cappbot_test.py
+++ cappbot_test.py
@@ -20,6 +20,7 @@ import imp
 import unittest
 
 from cappbot import CappBot
+import mini_github3
 
 
 class TestSequenceFunctions(unittest.TestCase):
@@ -30,7 +31,14 @@ class TestSequenceFunctions(unittest.TestCase):
         self.cappbot = CappBot(self.settings, self.database)
         # Replace the GitHub API with a mock.
         self.cappbot.github = Mock(spec=self.cappbot.github)
+        self.test_user = mini_github3.User(**{'_location': 'https://api.github.com/user', 'api_data': {'bio': None, 'public_gists': 0, 'name': 'CappBot', 'public_repos': 0, 'url': 'https://api.github.com/users/cappbot', 'created_at': '2011-09-02T16:59:16Z', 'html_url': 'https://github.com/cappbot', 'id': 1022439, 'blog': 'www.cappuccino.org', 'email': None, 'avatar_url': 'https://secure.gravatar.com/avatar/44790460d2e62628fc354296057f2b61?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png', 'followers': 0, 'location': 'Villa Straylight', 'gravatar_id': '44790460d2e62628fc354296057f2b61', 'following': 0, 'login': 'cappbot', 'hireable': False, 'type': 'User', 'company': None}, '_http': None, '_delivered': True, 'login': 'cappbot', '_etag': '"9a721b6d43903d25a6a90f73f5c5ddc7"'})
+        self.cappbot.github.current_user = Mock(return_value=self.test_user)
 
     def test_ensure_referenced_labels_exist(self):
         self.cappbot.ensure_referenced_labels_exist()
         self.cappbot.github.Labels.get_or_create_in_repository.assert_called_with("alice", "blox", "#new")
+
+    def test_current_user(self):
+        current_user = self.cappbot.current_user
+        self.cappbot.github.current_user.assert_called_once_with()
+        self.assertEquals(current_user, self.test_user)
