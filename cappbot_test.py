6bed5d192727e5978ab2e7f8a9284cc986a7f40c
Alexander Ljungberg <aljungberg@slevenbits.com>
2012-05-07 01:20:43 +0100

    Make unit test GitHub mocking process more reusable.

--- cappbot_test.py
+++ cappbot_test.py
@@ -25,6 +25,13 @@ from cappbot import CappBot
 import mini_github3
 
 
+def first(iterable):
+    try:
+        return next(iterable)
+    except StopIteration:
+        return None
+
+
 def load_fixture(name):
     with open(os.path.join(os.path.dirname(__file__), 'test_fixtures', name), 'rb') as inf:
         return json.load(inf)
@@ -50,47 +57,87 @@ class TestSequenceFunctions(unittest.TestCase):
         self.cappbot.github.current_user.assert_called_once_with()
         self.assertEquals(current_user, self.test_user)
 
-    def get_test_labels(self):
-        return mini_github3.Labels.from_dict(load_fixture('labels.json'))
-        #return mini_github3.Labels(entries=[mini_github3.Label(**label) for label in load_fixture('labels.json')])
+    def configure_github_mock(self, issues, labels, milestones, comments=None):
+        """Configure the GitHub API mock to make certain data available.
 
-    def get_test_issues(self, selection=None):
-        issues = load_fixture('issues.json')
-        if selection is not None:
-            issues = [issue for n, issue in enumerate(issues) if n in selection]
-        return mini_github3.Issues.from_dict(issues)
+        The parameters should be arrays of dicts from the JSON fixtures.
+        Comments should be an arrays of arrays of dicts.
+        """
 
-    def test_install_defaults(self):
-        labels = self.get_test_labels()
+        # There's quite a bit of GitHub interaction to fake.
+
+        comments = comments or {}
+        issues = mini_github3.Issues.from_dict(issues)
+        labels = mini_github3.Labels.from_dict(labels)
+        milestones = mini_github3.Milestones.from_dict(milestones)
+        comments = mini_github3.Comments.from_dict(comments)
+
         self.cappbot.github.Labels.by_repository = Mock(return_value=labels)
-        issues = self.get_test_issues(selection=[7])
-        milestone = mini_github3.Milestone(**load_fixture('milestone.json'))
 
-        def mock_patch(*args, **kwargs):
-            for k, v in kwargs.items():
-                # When you patch a milestone in GitHub V3 you send a numeric id but receive back a full
-                # milestone object.
-                if k == 'milestone' and v == milestone.number:
-                    v = milestone
-                # Similar when you patch the labels you send an array of strings but receive back an
-                # array of Label resources.
-                if k == 'labels' and v == ["#new"]:
-                    v = [labels[6]]
-                setattr(issues[0], k, v)
-        issues[0].patch = Mock(side_effect=mock_patch)
+        def install_issue_mock_patch(issue):
+            def mock_patch(*args, **kwargs):
+                for k, v in kwargs.items():
+                    # When you patch a milestone in GitHub V3 you send a numeric id but receive back a full
+                    # milestone object.
+                    if k == 'milestone':
+                        new_v = first(milestone for milestone in milestones if milestone.number == v)
+                        if not new_v:
+                            raise Exception("mock patch: no such milestone %s" % v)
+                        v = new_v
+
+                    # Similar when you patch the labels you send an array of strings but receive back an
+                    # array of Label resources.
+                    if k == 'labels':
+                        new_v = [first(label for label in labels if label.name == aName) for aName in v]
+                        if any(not label for label in new_v):
+                            raise Exception("mock patch: no such label(s) in %s" % v)
+                        v = new_v
+                    setattr(issue, k, v)
+            issue.patch = Mock(side_effect=mock_patch)
+
+        map(install_issue_mock_patch, issues)
+
+        def install_list_post_patch(a_list):
+            def mock_post(a_new_entry):
+                a_list.entries.append(a_new_entry)
+            a_list.post = Mock(side_effect=mock_post)
+        install_list_post_patch(issues)
+        install_list_post_patch(milestones)
+        install_list_post_patch(labels)
+        install_list_post_patch(comments)
+
+        def milestone_get_or_create_in_repository(user_name, repo_name, milestone_title):
+            for milestone in milestones:
+                if milestone.title == milestone_title:
+                    return milestone
+
+            milestone = mini_github3.Milestone()
+            milestone.title = milestone_title
+            milestones.post(milestone)  # Will call the Mock version.
+
+            return milestone
+
+        self.cappbot.github.Milestones.get_or_create_in_repository = Mock(side_effect=milestone_get_or_create_in_repository)
+
         self.cappbot.github.Issues.by_repository = Mock(return_value=issues)
 
-        self.cappbot.github.Milestones.get_or_create_in_repository = Mock(return_value=milestone)
+        for n, issue in enumerate(issues):
+            issue._mock_comments = mini_github3.Comments.from_dict(comments[n]) if n < len(comments) else mini_github3.Comments(entries=[])
+            install_list_post_patch(issue._mock_comments)
 
-        comments = mini_github3.Comments(entries=[])
-        comments.post = Mock()
-        self.cappbot.github.Comments.by_issue = Mock(return_value=comments)
+        def get_comments(issue):
+            return issue._mock_comments
 
-        fresh_comment = mini_github3.Comment()
-        self.cappbot.github.Comment = Mock(return_value=fresh_comment)
+        self.cappbot.github.Comments.by_issue = Mock(side_effect=get_comments)
+
+        return issues, labels, milestones, comments
+
+    def test_install_defaults(self):
+        issues, labels, milestones, comments = self.configure_github_mock(load_fixture('issues.json')[7:8], load_fixture('labels.json'), [load_fixture('milestone.json')])
 
         self.cappbot.run()
 
         issues[0].patch.assert_called_with(labels=['#new'], milestone=2)
-        comments.post.assert_called_with(fresh_comment)
-        self.assertEquals(fresh_comment.body, "**Milestone:** Someday.  **Label:** #new.  **What's next?** A reviewer should examine this issue.")
+        self.assertEquals(issues[0]._mock_comments[-1].body, "**Milestone:** Someday.  **Label:** #new.  **What's next?** A reviewer should examine this issue.")
+        issues[0]._mock_comments.post.assert_called_with(issues[0]._mock_comments[-1])
+
