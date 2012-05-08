66ea2dcc515d075ff857576964bf9e4986987a32
Alexander Ljungberg <aljungberg@slevenbits.com>
2012-05-08 16:57:35 +0100

    Move source into its own folder.

--- settings-sample.py
+++ settings-sample.py
@@ -1,6 +1,6 @@
 # See default_settings.py for setting names and explanations.
 
-from default_settings import *
+from main.default_settings import *
 
 GITHUB_USER = "cappbot"
 GITHUB_TOKEN = "12345678901234567890"
